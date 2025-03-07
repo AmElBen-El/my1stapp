77fc2345cd17df22380d365adac21d4c405a68bb
Moses Ayabiogbe <ayabiogbemoses@gmail.com>
2025-03-07 11:54:22 +0100

    1st React Native

--- App.js
+++ App.js
@@ -1,20 +1,39 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { ImageBackground, StyleSheet, Text, View, Image } from 'react-native';
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+   <ImageBackground source={require("./assets/A-React.png")}>
+      <View style={styles.container}>
+          <Text style={{fontSize: 30, color: "white", textAlign: 'center'}}>
+            React Native
+          </Text>      
+
+          <Text>
+            React Native is a framework for building mobile applications with JavaScript and React.
+          </Text>
+          <Image source={require("./assets/react1.png")} style={styles.img}/>
+        </View>
+   </ImageBackground>
   );
 }
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+  container:{
+    padding: 20,
+    paddingTop:60,
+    backgroundColor: '#9eb198',
+    height: "100%"    
   },
+text:{
+  fontSize: 18,
+  color: "#ffffff",
+  marginTop: 20,
+  borderWidth: 1,
+  borderColor: "#ffffff",
+  padding:10,
+  borderRadius:10,
+  backgroundColor:"#ffffff10",
+  width:"100"}
 });
+
+// assignment do an article that the scrollable
